07280235b2fa2c8dfbffa2cd2488960b192ddc76
Remi Beucherie <beucherie.r@gmail.com>
2019-11-05 16:33:22 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  article_title = Faker::Science.element
+  article_content = Faker::Beer.name
+  Article.create(title: article_title, content: article_content)
+end
